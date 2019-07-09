e7c33b37506b18407e7cc4a7b2e22b8921f3c6c1
Zohaib Butt <zbutt@muvaki.com>
2019-07-09 14:45:40 -0400

    support 0.12 syntax

--- main.tf
+++ main.tf
@@ -32,10 +32,16 @@ data "google_compute_image" "coreos" {
   family  = "${local.coreos_image_family}"
   project = "${local.coreos_project}"
 }
-
 resource "null_resource" "validate_restart_policy" {
-  count                                                                                                                                = "${local.invalid_restart_policy}"
-  "ERROR: Invalid `restart_policy` ${var.restart_policy} was provided. Must be one of `OnFailure`, `UnlessStopped`, `Always`, or `No`" = true
+  count = "${local.invalid_restart_policy}"
+
+  provisioner "local-exec" {
+    command = "echo ERROR: Invalid restart_policy ${var.restart_policy} was provided. Must be one of OnFailure, UnlessStopped, Always, or No"
+  }
+
+  provisioner "local-exec" {
+    command = "false"
+  }
 }
 
 data "external" "spec_as_yaml" {
